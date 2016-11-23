00356fc326d0083bb299c33d00e0adc2ccb6d5f3
Denis Michailov <denis.michailov@gmail.com>
2016-11-23 11:08:51 +1000

    add pug, improve sass with log errors. rename folders to app and public.

--- gulpfile.js
+++ gulpfile.js
@@ -5,55 +5,52 @@ var rename       = require('gulp-rename');
 var sass         = require('gulp-sass');
 var cleanCSS     = require('gulp-clean-css');
 var autoPrefixer = require('gulp-autoprefixer');
-var jade		 = require('gulp-jade');
+var pug			 = require('gulp-pug');
 var livereload   = require('gulp-livereload');
 var connect      = require('gulp-connect');
 
 gulp.task('js', function(){
-	gulp.src('src/js/*.js')
+	gulp.src('app/js/*.js')
 		.pipe(connect.reload())
-		.pipe(gulp.dest('dist/js/'));
+		.pipe(gulp.dest('public/js/'));
 });
 
 gulp.task('sass', function() {
-	gulp.src('src/sass/main.sass')
-    .pipe(sass())
+	gulp.src('app/styles/main.sass')
+	.pipe(sass().on('error', sass.logError))
 	.pipe(autoPrefixer({
 			browsers: ['last 2 versions', '> 1%', 'IE 8'],
 			cascade: false
 		}))
 	.pipe(cleanCSS({compatibility: 'ie8'}))
 	.pipe(rename('bundle.min.css'))
-    .pipe(gulp.dest('dist/css/'))
+	.pipe(gulp.dest('public/css/'))
 	.pipe(connect.reload());
 });
 
 gulp.task('connect', function() {
   connect.server({
-    root: '.',
-    livereload: true,
+	root: './public',
+	livereload: true,
 	port: 8080
   });
 });
 
-gulp.task('jade', function(){
-	gulp.src('src/jade/**/*.jade')
-		.pipe(jade({ pretty:true }))
-		.pipe(gulp.dest('./'))
+gulp.task('pug', function() {
+	return gulp.src('app/views/*.pug')
+		.pipe(pug({
+			pretty:true
+		}))
+		.pipe(gulp.dest('./public'))
 		.pipe(connect.reload());
 });
 
-// gulp.task('html', function(){
-// 	gulp.src('index.html')
-// 		.pipe(connect.reload());
-// });
 
 gulp.task('watch', function(){
-	gulp.watch('src/sass/**/*.sass', ['sass']);
-	gulp.watch('src/sass/**/*.scss', ['sass']);
-	gulp.watch('src/jade/**/*.jade', ['jade']);
-	gulp.watch('src/jade/**/*.html', ['jade']);
-	gulp.watch('src/js/*.js', ['js']);
+	gulp.watch('app/styles/**/*.sass', ['sass']);
+	gulp.watch('app/styles/**/*.scss', ['sass']);
+	gulp.watch('app/views/**/*.*', ['pug']);
+	gulp.watch('app/js/*.js', ['js']);
 });
 
-gulp.task('default', ['connect', 'jade', 'js', 'sass', 'watch']);
+gulp.task('default', ['connect', 'pug', 'js', 'sass', 'watch']);
